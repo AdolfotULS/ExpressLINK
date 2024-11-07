43143b031b1549939df9acfc8b985d82b815cbd6
Adolfo <166766031+AdolfotULS@users.noreply.github.com>
2024-11-07 15:05:17 -0300

    Inicializacion

---
+++ src/expresslink/model/Usuario.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package expresslink.model;
+
+/**
+ *
+ * @author SheratoD
+ */
+public class Usuario {
+    private String nombre;
+
+    public Usuario(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
