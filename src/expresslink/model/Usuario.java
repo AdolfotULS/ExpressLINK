9d9794c40dc8a7c383c20fbd84497bebeadb551e
IgnaciaMiranda <166965387+IgnaciaMiranda@users.noreply.github.com>
2024-11-08 17:01:17 -0300

    Avance 2
    Implementacion del constructor, get, set y el toString

--- src/expresslink/model/Usuario.java
+++ src/expresslink/model/Usuario.java
@@ -14,4 +14,66 @@ public class Usuario {
         SUCURSAL,
         ADMIN
     }
+//Constructor
+    public Usuario(int identificador, String nombre, String correo, String contrasenia, String telefono) {
+        this.identificador = identificador;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contrasenia = contrasenia;
+        this.telefono = telefono;
+    }
+//Get
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+    //Set
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+    @Override
+    
+    public String toString() {
+        return "Usuario{" +
+               "identificador=" + identificador +
+               ", nombre='" + nombre + '\'' +
+               ", correo='" + correo + '\'' +
+               ", contrasenia='" + contrasenia + '\'' +
+               ", telefono='" + telefono + '\'' +
+               '}';
+    }
 }
